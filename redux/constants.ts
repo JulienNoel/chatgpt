cb50fb268c7b121c04092af841a774f6e443dc5a
Julien Noel <jncoweb@gmail.com>
2023-07-05 23:15:23 +0200

    wip reducer slice for messages

---
+++ redux/constants.ts
@@ -0,0 +1,4 @@
+export const GET_MESSAGES = 'messages/GET_MESSAGES'
+export const CREATE_MESSAGE = 'messages/CREATE_MESSAGE'
+export const UPDATE_MESSAGE = 'messages/UPDATE_MESSAGE'
+export const DELETE_MESSAGE = 'messages/DELETE_MESSAGE'
--- redux/messages/messages.slice.ts
+++ redux/messages/messages.slice.ts
@@ -7,21 +7,15 @@ type MessagesState = {
 
 const initialState: MessagesState = { value: 0 }  
 
-const counterSlice = createSlice({
+const messageSlice = createSlice({
   name: 'messages',
   initialState,
   reducers: {
-    increment(state) {
-      state.value++
-    },
-    decrement(state) {
-      state.value--
-    },
-    incrementByAmount(state, action: PayloadAction<number>) {
-      state.value += action.payload
-    },
+    createPost(state, action) {},
+    updatePost(state, action) {},
+    deletePost(state, action) {},
   },
 })
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
-export default counterSlice.reducer
+export const { createPost, updatePost, deletePost } = messageSlice.actions
+export default messageSlice.reducer
