99447d3c62618e2ba04047dfeab0064c71c9faf8
Julien Noel <jncoweb@gmail.com>
2023-07-06 23:07:20 +0200

    wip reducer and ts config + eslint

--- redux/messages/messages.slice.ts
+++ redux/messages/messages.slice.ts
@@ -1,21 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 
-type MessagesState = {
-  value: number
+type TMessages = {
+  message: string,
 }
 
-const initialState: MessagesState = { value: 0 }  
+type TMessageState = Partial<Record<string, TMessages>>
 
-const messageSlice = createSlice({
+const initialState: TMessageState = { }  
+
+export const messageSlice = createSlice({
   name: 'messages',
   initialState,
   reducers: {
-    createPost(state, action) {},
-    updatePost(state, action) {},
-    deletePost(state, action) {},
+    createPost(state: TMessageState, action: PayloadAction<Record<string, TMessages>>) {
+      ...state,
+      ...action.payload
+    },
+    updatePost(state: TMessageState, action: PayloadAction<TMessages>) {
+
+    },
+    deletePost(state: TMessageState, action: PayloadAction<string>) {
+      
+    },
   },
 })
 
 export const { createPost, updatePost, deletePost } = messageSlice.actions
-export default messageSlice.reducer
